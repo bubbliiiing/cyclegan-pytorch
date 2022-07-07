812ff329aebe60a9b085ddf378cdf44599a0450f
Bubbliiiing <3323290568@qq.com>
2022-07-07 15:52:17 +0800

    update show_config

--- cyclegan.py
+++ cyclegan.py
@@ -37,8 +37,7 @@ class CYCLEGAN(object):
         self.__dict__.update(self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)  
-            setattr(self._defaults, name, value)  
-            
+            self._defaults[name] = value 
         self.generate()
         
         show_config(**self._defaults)
